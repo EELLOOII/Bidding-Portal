4dcc745a8ae184cb3dd83f399279d4b97850a678
eloicram18 <eloicram18@gmail.com>
2025-01-17 16:42:10 +0800

    CHORE: updates the signup page

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,16 +1,7 @@
 <template>
     <div>
         <h2>Sign Up</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil recusandae et ipsam voluptas nisi deleniti ut doloribus, quo consequuntur adipisci eum laborum numquam? Exercitationem accusantium similique suscipit nemo. Iure, neque.
-        Cupiditate nihil repudiandae molestiae dolor ut cumque similique architecto, voluptate nobis maiores atque minima tempora ipsa nisi voluptatibus maxime vel. Assumenda repellat soluta veritatis atque ut blanditiis omnis, minima ducimus.
-        Modi beatae voluptatem inventore suscipit expedita voluptate dolorem, quas delectus ratione possimus consequatur consectetur recusandae voluptatum nobis unde magnam minima, consequuntur amet totam non culpa quae atque dolore? Praesentium, nobis.
-        Similique mollitia aperiam ipsam cupiditate quibusdam! Dolorem atque porro labore in perferendis tenetur ratione soluta ea, architecto adipisci quas nostrum. Consequatur inventore esse ea est harum alias reiciendis eos praesentium?
-        Veniam facere architecto possimus assumenda voluptates porro ratione totam veritatis maxime fuga voluptatum illum ipsum inventore unde quasi error neque velit, culpa voluptatem repellendus officiis et nihil! Sapiente, eius veritatis.
-        Consequuntur odit saepe sunt unde incidunt, blanditiis, commodi dolore possimus cum natus explicabo minus. Animi quis sunt, itaque ducimus laborum consequuntur eos cumque facere tempore ex quam aspernatur adipisci repellat.
-        Cumque velit debitis illum nulla doloribus. Soluta quisquam veniam sequi dolores natus vitae, debitis facere harum consectetur deleniti asperiores sunt itaque sit adipisci reiciendis? Consequuntur voluptates cumque quis doloribus ad.
-        Omnis eaque id quidem odit voluptatem ipsa illo, ipsam blanditiis obcaecati repudiandae, perspiciatis similique adipisci ex distinctio libero eligendi cum cumque neque laborum tenetur, velit facere. Quis voluptate velit exercitationem.
-        Nisi corrupti quos veniam sapiente provident porro vero deserunt accusantium accusamus aliquid distinctio, doloremque dicta optio modi? Sint veniam soluta magni nisi possimus blanditiis eos nulla dolorum eligendi? Vel, quia.
-        Atque aperiam cupiditate ab nisi tempora repudiandae minima incidunt nulla nostrum esse, quas laborum velit minus quod? Accusantium voluptas ex sint doloremque est, possimus fugit hic illum porro debitis exercitationem?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda reprehenderit accusamus praesentium ut. Dolorem quaerat voluptate repellat quasi similique aut quis debitis, facere dolores illo, necessitatibus dicta omnis saepe impedit!</p>
     </div>
 </template>
 
