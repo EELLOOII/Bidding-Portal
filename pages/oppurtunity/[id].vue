30825f374aa3dc252b85338b99812fdfa7a2d53f
eloicram18 <eloicram18@gmail.com>
2025-01-17 15:15:14 +0800

    Initial commit

---
+++ pages/oppurtunity/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Oppurtunity ID {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae explicabo exercitationem velit deserunt doloremque asperiores quibusdam cupiditate repellendus vero eveniet! Quae eos architecto animi delectus nulla quasi nemo ducimus optio accusamus omnis officiis commodi, cupiditate velit, fugit voluptatem vel sunt.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
